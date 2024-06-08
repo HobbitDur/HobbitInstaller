803ce30b74b5d73218a40ec2795db45f017f1ff8
LudovicGuerra <ludovic791@gmail.com>
2024-06-08 20:05:47 +0200

    bugfix reloaded and ragnarok

--- hobbitgui.py
+++ hobbitgui.py
@@ -3,7 +3,7 @@ import os
 from PyQt6.QtCore import Qt, QCoreApplication, QThreadPool, QRunnable, QObject, pyqtSignal, pyqtSlot, QThread
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QCheckBox, QMessageBox, QProgressDialog, QMainWindow, QProgressBar, QRadioButton, \
-    QLabel, QFrame, QStyle, QSizePolicy
+    QLabel, QFrame, QStyle, QSizePolicy, QButtonGroup
 
 from modmanager import ModManager
 
@@ -23,6 +23,8 @@ class Installer(QObject):
 class WindowInstaller(QWidget):
     install_requested = pyqtSignal(ModManager, list, bool, dict, bool)
     FF8_RELOAD_NAME = "FFVIII-Reloaded-FR-ONLY"
+    RAGNAROK_NAME = "Ragnarok-EN-ONLY"
+    LIST_SPECIAL_MOD = [FF8_RELOAD_NAME, RAGNAROK_NAME]
     MOD_CHECK_DEFAULT = ['FFNx', 'FFNxFF8Music']
     def __init__(self, mod_manager, icon_path='ModSetup'):
 
@@ -61,15 +63,17 @@ class WindowInstaller(QWidget):
         self.label_mod = QLabel("Mod selection")
         for mod_name in self.mod_manager.mod_file_list:
             if self.mod_manager.mod_dict[mod_name]['type'] == 'github':
-                self.mod_checkbox[mod_name] = QCheckBox(mod_name)
+                self.mod_checkbox[mod_name] = QCheckBox(parent=self, text=mod_name)
             elif self.mod_manager.mod_dict[mod_name]['type'] == 'direct_link':
-                self.mod_checkbox[mod_name] = QCheckBox(mod_name)
+                self.mod_checkbox[mod_name] = QCheckBox(parent=self, text=mod_name)
             else:
                 print("No type found for mod {} with value {}".format(mod_name, self.mod_manager.mod_dict[mod_name]))
             if mod_name in self.MOD_CHECK_DEFAULT:
                 self.mod_checkbox[mod_name].setChecked(True)
             if mod_name == self.FF8_RELOAD_NAME:
                 self.mod_checkbox[mod_name].toggled.connect(self.activate_ff8reload)
+            elif mod_name == self.RAGNAROK_NAME:
+                self.mod_checkbox[mod_name].toggled.connect(self.activate_ragnarok)
 
         # Button install
         self.install_button = QPushButton(parent=self, text="Install")
@@ -80,23 +84,41 @@ class WindowInstaller(QWidget):
         self.install_over.hide()
 
         # FFVIII Reloaded
+
         self.ff8reloaded_classic = QRadioButton(parent=self, text="FF8 Reloaded Classic")
         self.ff8reloaded_classic.setChecked(True)
         self.ff8reloaded_classic.toggled.connect(lambda: self.ff8reloadedstate(self.ff8reloaded_classic))
         self.ff8reloaded_classic.hide()
-
         self.ff8reloaded_level1 = QRadioButton(parent=self,  text="FF8 Reloaded Level 1")
         self.ff8reloaded_level1.toggled.connect(lambda: self.ff8reloadedstate(self.ff8reloaded_level1))
         self.ff8reloaded_level1.hide()
         self.ff8reloaded_level100 = QRadioButton(parent=self,  text="FF8 Reloaded Level 100")
         self.ff8reloaded_level100.toggled.connect(lambda: self.ff8reloadedstate(self.ff8reloaded_level100))
         self.ff8reloaded_level100.hide()
+        self.parent_ff8reloaded = QButtonGroup(self)
+        self.parent_ff8reloaded.addButton(self.ff8reloaded_classic)
+        self.parent_ff8reloaded.addButton(self.ff8reloaded_level1)
+        self.parent_ff8reloaded.addButton(self.ff8reloaded_level100)
+
+        # Ragnarok
+        self.ragnarok_standard = QRadioButton(parent=self, text="Ragnarok standard")
+        self.ragnarok_standard.setChecked(True)
+        self.ragnarok_standard.toggled.connect(lambda: self.ragnarokstate(self.ragnarok_standard))
+        self.ragnarok_standard.hide()
+        self.ragnarok_lionheart = QRadioButton(parent=self,  text="Ragnarok lionheart")
+        self.ragnarok_lionheart.toggled.connect(lambda: self.ragnarokstate(self.ragnarok_lionheart))
+        self.ragnarok_lionheart.hide()
+        self.parent_ragnarok = QButtonGroup(self)
+        self.parent_ragnarok.addButton(self.ragnarok_standard)
+        self.parent_ragnarok.addButton(self.ragnarok_lionheart)
 
         self.progress = QProgressBar(parent=self)
 
         self.layout_main = QVBoxLayout()
         self.layout_setup = QVBoxLayout()
         self.layout_mod = QVBoxLayout()
+        self.layout_ragnarok = QVBoxLayout()
+        self.layout_ff8reloaded = QVBoxLayout()
         self.setup_layout()
         self.show_all()
 
@@ -110,8 +132,18 @@ class WindowInstaller(QWidget):
             self.ff8reloaded_level1.hide()
             self.ff8reloaded_level100.hide()
         self.resize(self.minimumSizeHint())
+    def activate_ragnarok(self):
+        if self.mod_checkbox[self.RAGNAROK_NAME].isChecked():
+            self.ragnarok_standard.show()
+            self.ragnarok_lionheart.show()
+        else:
+            self.ragnarok_standard.hide()
+            self.ragnarok_lionheart.hide()
+        self.resize(self.minimumSizeHint())
 
     def ff8reloadedstate(self, b):
+        pass
+    def ragnarokstate(self, b):
         pass
 
     def show_all(self):
@@ -131,15 +163,27 @@ class WindowInstaller(QWidget):
 
         self.layout_mod.addWidget(self.label_mod)
         for key, mod in self.mod_checkbox.items():
-            self.layout_mod.addWidget(mod)
-        self.layout_mod.addWidget(self.ff8reloaded_classic)
-        self.layout_mod.addWidget(self.ff8reloaded_level1)
-        self.layout_mod.addWidget(self.ff8reloaded_level100)
-        self.layout_mod.addWidget(self.install_button)
-        self.layout_mod.addWidget(self.progress)
+            if key not in self.LIST_SPECIAL_MOD:
+                self.layout_mod.addWidget(mod)
+
+
+        self.layout_ff8reloaded.addWidget(self.mod_checkbox[self.FF8_RELOAD_NAME])
+        self.layout_ff8reloaded.addWidget(self.ff8reloaded_classic)
+        self.layout_ff8reloaded.addWidget(self.ff8reloaded_level1)
+        self.layout_ff8reloaded.addWidget(self.ff8reloaded_level100)
+
+        self.layout_ragnarok.addWidget(self.mod_checkbox[self.RAGNAROK_NAME])
+        self.layout_ragnarok.addWidget(self.ragnarok_standard)
+        self.layout_ragnarok.addWidget(self.ragnarok_lionheart)
+
+        self.layout_mod.addLayout(self.layout_ragnarok)
+        self.layout_mod.addLayout(self.layout_ff8reloaded)
 
         self.layout_main.addLayout(self.layout_setup)
         self.layout_main.addLayout(self.layout_mod)
+        self.layout_main.addWidget(self.install_button)
+        self.layout_main.addWidget(self.progress)
+
         self.setLayout(self.layout_main)
         self.resize(self.minimumSizeHint())
 
@@ -157,6 +201,11 @@ class WindowInstaller(QWidget):
                     special_status[self.FF8_RELOAD_NAME] = self.ff8reloaded_level1.text()
                 elif self.ff8reloaded_level100.isChecked():
                     special_status[self.FF8_RELOAD_NAME] = self.ff8reloaded_level100.text()
+            elif mod_name == self.RAGNAROK_NAME:
+                if self.ragnarok_standard.isChecked():
+                    special_status[self.RAGNAROK_NAME] = self.ragnarok_standard.text()
+                elif self.ragnarok_lionheart.isChecked():
+                    special_status[self.RAGNAROK_NAME] = self.ragnarok_lionheart.text()
             if self.mod_checkbox[mod_name].checkState() == Qt.CheckState.Checked:
                 mod_to_be_installed.append(mod_name)
         self.progress.setRange(0, len(mod_to_be_installed))
